85354c1924b2c01d844d8225dbb001f00bf8326f
Aakash Kumar Patle <aakashpatle1009@gmail.com>
2025-12-25 00:06:04 +0530

    deleting extra file

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base API URL
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:9000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 // Create axios instance
 const axiosInstance = axios.create({
